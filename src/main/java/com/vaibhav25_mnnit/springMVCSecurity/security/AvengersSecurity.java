014655fd259ee17bbf53129f61e8a1a06025e2aa
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2025-02-15 22:47:21 +0530

    added InMemoryUserDetailsManager for creating the users,their password and their rols

---
+++ src/main/java/com/vaibhav25_mnnit/springMVCSecurity/security/AvengersSecurity.java
@@ -0,0 +1,60 @@
+package com.vaibhav25_mnnit.springMVCSecurity.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AvengersSecurity {
+
+    @Bean
+    public InMemoryUserDetailsManager avengerDetailManger() {
+
+        UserDetails ironman = User.builder()
+                .username("ironman")
+                .password("{noop}ironman")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+
+
+        UserDetails captain = User.builder()
+                .username("captain")
+                .password("{noop}captain")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+
+
+        UserDetails thor = User.builder()
+                .username("thor")
+                .password("{noop}thor")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails hulk = User.builder()
+                .username("hulk")
+                .password("{noop}hulk")
+                .roles("EMPLOYEE")
+                .build();
+
+
+        UserDetails blackwidow = User.builder()
+                .username("blackwidow")
+                .password("{noop}blackwidow")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails hawkeye = User.builder()
+                .username("hawkeye")
+                .password("{noop}hawkeye")
+                .roles("EMPLOYEE")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(ironman,captain,thor,hulk,blackwidow,hawkeye);
+
+    }
+}
