72c12126f7ee89ccec4a89291491ea97dfeddb7d
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2025-02-16 17:08:15 +0530

    added database support to read user details 1.we need to create two table there a.users and b.authorities with proper coulmn names and foreigh key mapping

--- src/main/java/com/vaibhav25_mnnit/springMVCSecurity/security/AvengersSecurity.java
+++ src/main/java/com/vaibhav25_mnnit/springMVCSecurity/security/AvengersSecurity.java
@@ -8,53 +8,62 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AvengersSecurity {
 
+//    @Bean
+//    public InMemoryUserDetailsManager avengerDetailManger() {
+//
+//        UserDetails ironman = User.builder()
+//                .username("ironman")
+//                .password("{noop}ironman")
+//                .roles("AVENGER","MANAGER","ADMIN")
+//                .build();
+//
+//
+//        UserDetails captain = User.builder()
+//                .username("captain")
+//                .password("{noop}captain")
+//                .roles("AVENGER","MANAGER")
+//                .build();
+//
+//
+//        UserDetails thor = User.builder()
+//                .username("thor")
+//                .password("{noop}thor")
+//                .roles("AVENGER")
+//                .build();
+//
+//        UserDetails hulk = User.builder()
+//                .username("hulk")
+//                .password("{noop}hulk")
+//                .roles("AVENGER")
+//                .build();
+//
+//
+//        UserDetails blackwidow = User.builder()
+//                .username("blackwidow")
+//                .password("{noop}blackwidow")
+//                .roles("AVENGER")
+//                .build();
+//
+//        UserDetails hawkeye = User.builder()
+//                .username("hawkeye")
+//                .password("{noop}hawkeye")
+//                .roles("AVENGER")
+//                .build();
+//        return new InMemoryUserDetailsManager(ironman,captain,thor,hulk,blackwidow,hawkeye);
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager avengerDetailManger() {
-
-        UserDetails ironman = User.builder()
-                .username("ironman")
-                .password("{noop}ironman")
-                .roles("AVENGER","MANAGER","ADMIN")
-                .build();
-
-
-        UserDetails captain = User.builder()
-                .username("captain")
-                .password("{noop}captain")
-                .roles("AVENGER","MANAGER")
-                .build();
-
-
-        UserDetails thor = User.builder()
-                .username("thor")
-                .password("{noop}thor")
-                .roles("AVENGER")
-                .build();
-
-        UserDetails hulk = User.builder()
-                .username("hulk")
-                .password("{noop}hulk")
-                .roles("AVENGER")
-                .build();
-
-
-        UserDetails blackwidow = User.builder()
-                .username("blackwidow")
-                .password("{noop}blackwidow")
-                .roles("AVENGER")
-                .build();
-
-        UserDetails hawkeye = User.builder()
-                .username("hawkeye")
-                .password("{noop}hawkeye")
-                .roles("AVENGER")
-                .build();
-        return new InMemoryUserDetailsManager(ironman,captain,thor,hulk,blackwidow,hawkeye);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
